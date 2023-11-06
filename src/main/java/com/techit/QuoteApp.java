dba2c1727c18e6ee24efbf1d08cdd365aab6cabe
daseul-jang <devzzang94@gmail.com>
2023-11-06 10:44:12 +0900

    5단계: 목록

--- src/main/java/com/techit/QuoteApp.java
+++ src/main/java/com/techit/QuoteApp.java
@@ -1,5 +1,9 @@
 package com.techit;
 
+import com.techit.dto.QuoteDto;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class QuoteApp {
@@ -7,6 +11,7 @@ public class QuoteApp {
     public void run() {
         System.out.println("== 명언 앱 ==");
 
+        List<QuoteDto> quoteList = new ArrayList<>();
         int num = 1;
 
         while (true) {
@@ -21,10 +26,25 @@ public class QuoteApp {
                     String quoteTxt = sc.nextLine();
 
                     System.out.print("작가 : ");
-                    String author = sc.nextLine();
+                    String quoteAuthor = sc.nextLine();
 
+                    QuoteDto quoteDto = new QuoteDto(num, quoteTxt, quoteAuthor);
+                    quoteList.add(quoteDto);
                     System.out.printf("%d 번 명언이 등록되었습니다.\n", num);
                     num++;
+
+                    break;
+                case "목록" :
+                    System.out.println("  번호  |    작가    |          명언          ");
+                    System.out.println("================================================");
+
+                    for(int i = quoteList.size() - 1; i >= 0; i--) {
+                        int no = quoteList.get(i).getQuoteNo();
+                        String author = quoteList.get(i).getQuoteAuthor();
+                        String text = quoteList.get(i).getQuoteTxt();
+
+                        System.out.printf("   %d    |    %s    |  %s \n", no, author, text);
+                    }
 
                     break;
             }
