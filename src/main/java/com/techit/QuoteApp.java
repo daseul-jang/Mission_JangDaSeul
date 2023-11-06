c139f40419550290979ecc32f06e1b7673730230
daseul-jang <devzzang94@gmail.com>
2023-11-06 10:03:58 +0900

    1단계: 종료

---
+++ src/main/java/com/techit/QuoteApp.java
@@ -0,0 +1,11 @@
+package com.techit;
+
+import java.util.Scanner;
+
+public class QuoteApp {
+    Scanner sc = new Scanner(System.in);
+    public void run() {
+        System.out.print("== 명언 앱 == \n명령) ");
+        sc.nextLine();
+    }
+}
