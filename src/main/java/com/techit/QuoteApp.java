8e083008e49df4ebf800aaab16a6d300dfa58450
daseul-jang <devzzang94@gmail.com>
2023-11-06 10:21:10 +0900

    2단계: 등록

--- src/main/java/com/techit/QuoteApp.java
+++ src/main/java/com/techit/QuoteApp.java
@@ -5,7 +5,23 @@ import java.util.Scanner;
 public class QuoteApp {
     Scanner sc = new Scanner(System.in);
     public void run() {
-        System.out.print("== 명언 앱 == \n명령) ");
-        sc.nextLine();
+        System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = sc.nextLine();
+
+            switch (cmd) {
+                case "종료" :
+                    return;
+                case "등록" :
+                    System.out.print("명언 : ");
+                    String quoteTxt = sc.nextLine();
+
+                    System.out.print("작가 : ");
+                    String author = sc.nextLine();
+                    break;
+            }
+        }
     }
 }
